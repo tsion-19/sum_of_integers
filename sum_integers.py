7e95c4628204073dc9480fb88bfa249c6bb2629e
tsion-19 <83315934+tsion-19@users.noreply.github.com>
2023-10-08 23:01:26 -0700

    Update sum_integers.py

--- sum_integers.py
+++ sum_integers.py
@@ -2,15 +2,17 @@
 total = 0
 
 while True:
+   
+    print('\nEOFError occurred')
     try:
-        num = int(input("Enter a positive integer (0 to stop): "))
+        num =int(input("Enter a positive integer (0 to stop): "))
         if num == 0:
             break
         elif num < 0:
             print("Please enter a positive integer.")
         else:
             total += num
-    except ValueError:
+    except EOFError as e:
         print("Invalid input. Please enter a positive integer.")
 
 print("Sum of the positive integers entered:", total)
